4dd2593dd37149fbc55eff632972b9bf0bacd3e5
vas <vas.grishko@yandex.ru>
2023-03-13 18:49:33 +0300

    Изменили файл View

--- View.py
+++ View.py
@@ -1,43 +1,51 @@
-import view
-import model
+def new_rec(mode="new") -> tuple:
+    if mode == "new":
+        print("Режим ввода новой записи ")
+    elif mode == "update":
+        print("Ввод новых данных для записи {surname}")
+        print("Пустая строка означает оставить данные без изменения")
+    else:
+        raise ValueError(f"Недоустимый флаг mode: {mode}")
+    surname = input("Введите фамилию: ")
+    name = input("Введите имя: ")
+    tel = input("Введите номер телефона: ")
+    desc = input("Введите описание: ")
+    return surname, name, tel, desc
 
 
-def menu():
-    phone_book_main = []
-    while True:
-        choice = view.show_menu()
-        if choice == "":
-            print("До свидания!")
-            break
-        elif choice == "1":
-            rec = model.create_rec(*view.new_rec(mode="new"))
-            phone_book_main.append(rec)
-        elif choice == "2":
-            surname = view.surname()
-            recs = model.select(phone_book_main, surname)
-            view.show_recs(recs)
-        elif choice == "3":
-            surname = view.surname()
-            recs = model.select(phone_book_main, surname)
-            if recs:
-                idx = phone_book_main.index(recs[0])
-                rec = model.create_rec(*view.new_rec(mode="update"))
-                rec = model.merge(rec, recs[0])
-                phone_book_main[idx] = rec
-        elif choice == "4":
-            surname = view.surname()
-            recs = model.select(phone_book_main, surname)
-            if recs:
-                idx = phone_book_main.index(recs[0])
-                phone_book_main.pop(idx)
-        elif choice == "5":
-            filename = view.file_name()
-            recs = model.import_file(filename)
-            phone_book_main.extend(recs)
-        elif choice == "6":
-            filename = view.file_name()
-            model.export_file(filename, phone_book_main)
-        elif choice == "7":
-            view.show_all_recs(phone_book_main)
-        else:
-            print("Не правильный ввод")
+def file_name() -> str:
+    return input("Введите имя файла: ")
+
+
+def surname():
+    return input("Введите фамилию: ")
+
+
+def show_recs(recs: list):
+    for rec in recs:
+        show_rec(rec)
+
+
+def show_rec(rec: dict):
+    for val in rec.values():
+        print(val, end=" ")
+    print("")
+
+
+def show_all_recs(phone_book: list):
+    print("Справочник: ")
+    for rec in phone_book:
+        show_rec(rec)
+
+
+def show_menu() -> str:
+    print("_"*55)
+    print("\n\tМЕНЮ:")
+    print("\t(1)create - Ввод новой записи:")
+    print("\t(2)read   - Поиск записи по фамилии")
+    print("\t(3)udate  - Изменение записи по фамилии")
+    print("\t(4)delete - Удаление записи по фамилии")
+    print("\t(5)import - Импорт из файла")
+    print("\t(6)export - Экспорт в файл")
+    print("\t(7)show   - Вывод на экран содержиого справочника\n")
+    return input("Выберите нужный пункт: ")
